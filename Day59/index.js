aa2131952f99868deaf555e1ae3c3686cad020eb
Rajat saraswat <149191138+kg-rajat@users.noreply.github.com>
2024-05-21 15:05:27 +0530

    Add files via upload
    day 59

---
+++ Day59/index.js
@@ -0,0 +1,88 @@
+let pencilPrice = 10;
+let erasorPrice = 5;
+
+
+console.log("The total price: ", pencilPrice + erasorPrice);
+
+// Template Literals
+
+console.log(`the total price: ${pencilPrice + erasorPrice}`);
+
+// Arithmetic Operators
+
+let a = 40;
+let b = 10;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a ** b);
+console.log(a % b);
+console.log(a**b);
+
+// String practice
+
+// String Indices
+
+let fullName = "Rajat saraswat";
+
+console.log(fullName[0]);
+console.log(fullName[1]);
+console.log(fullName[2]);
+console.log(fullName[3]);
+console.log(fullName[4]);
+console.log(fullName[5]);
+console.log(fullName[6]);
+console.log(fullName[7]);
+console.log(fullName[8]);
+console.log(fullName[9]);
+console.log(fullName[10]);
+console.log(fullName[11]);
+console.log(fullName[12]);
+
+
+console.log(fullName[100]);
+
+console.log(fullName.length);
+
+let firstName = "Rajat";
+let lastName = "saraswat";
+
+let full_name = firstName + " " + lastName;
+console.log(full_Name);
+
+let name = "Geeta";
+console.log(name.length);
+
+let first_Name = "Rahul";
+console.log(first_Name[0]);
+console.log(first_Name[4]);
+
+let x = "apnacollege";
+let y = 123;
+
+let z = x + y;
+
+console.log(z);
+
+
+const firstname = "John";
+const lastname = "Doe";
+const fullname = firstname + " " + lastname;
+console.log(fullname);
+
+// Get the length of a string
+const message = "Hello, world!";
+const messageLength = message.length;
+console.log(messageLength);
+
+// Get the character at a specific index in a string
+const letter = message.charAt(0);
+console.log(letter);
+
+// Find the first occurrence of a substring in a string
+const index = message.indexOf("world");
+
+// Replace all occurrences of a substring in a string with another substring
+const newMessage = message.replace("world", "universe");

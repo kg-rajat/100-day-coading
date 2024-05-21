e87a38e4a8025e3b0cf2b3fb5fc8344eabba527a
Rajat saraswat <149191138+kg-rajat@users.noreply.github.com>
2024-05-21 14:42:48 +0530

    Add files via upload
    day 49

---
+++ Day49/index.js
@@ -0,0 +1,17 @@
+// variables declare
+let num1 = 10;
+let num2 = 5;
+
+//perform arithmetic opeartions
+let sum = num1 + num2;
+let different = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+let remainder = num1 % num2;
+
+
+console.log("Sum:", sum);
+console.log("Difference:", Difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
+console.log("Remainder:", remainder);

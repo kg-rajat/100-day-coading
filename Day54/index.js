43e75ab40722f3e98bb2205e6c65f32bf34a8a0b
Rajat saraswat <149191138+kg-rajat@users.noreply.github.com>
2024-05-21 14:57:22 +0530

    Add files via upload
    day54

---
+++ Day54/index.js
@@ -0,0 +1,33 @@
+// Q1 - Print the number from 0 to 15
+
+for (let i = 1; i<15; i++){
+  console.log("Number: ",i);
+}
+
+//Q2 - Print the number from 12 to 24
+
+for ( let i= 12; i<= 24; i++){
+  console.log("Number: ", i);
+}
+
+//Q3 - Print the ODD number from 7 to 31
+
+for (let i = 7; i <= 31; i++) {
+  if (i % 2 !== 0 ){
+    document.log(i)
+  }
+}
+
+//Q4 - Print the EVEN number from 10 to -20
+
+for (let i=10; i>= -20; i--){
+  if (i % 2 === 0){
+    console.log(i)
+  }
+}
+
+// for (let i = 10; i>= -20; i--){
+//if (i % 2 == 0){
+//console.log(i)
+//  }
+// }

82d7fbc9ab74677191725b4131dec0b9af6e021c
Rajat saraswat <149191138+kg-rajat@users.noreply.github.com>
2024-05-21 14:36:23 +0530

    Add files via upload
    Day 45

---
+++ Day45/index.js
@@ -0,0 +1,7 @@
+const accountId = 1316
+let accountEmail = "rajatsaraswat64@gmail.com"
+var accountPassword= "Rjt@123"
+accountCity="Mathura"
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, location, accountState])

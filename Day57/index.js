475f4f245625a1712c4214c0dd6f76a24d40938f
Rajat saraswat <149191138+kg-rajat@users.noreply.github.com>
2024-05-21 15:00:33 +0530

    Add files via upload
    day 57

---
+++ Day57/index.js
@@ -0,0 +1,32 @@
+// Question 1 Print the number from 0 to 15
+
+for (let i = 1; i <=15; i++) {
+  console.log("Number: ", i);
+}
+
+// Q2 -Print the number from 12  to 24
+for (let a = 12; a <= 24; i++) {
+  console.log("Number ", i);
+}
+
+// Q3 - Print the odd number from 7 to 31
+
+for (let i = 7; i <=31; i++) {
+  if (i % 2 !==0){
+    console.log(i)
+  }
+}
+
+//Q4 - Print the even number from 10 to 20
+
+for( let i=10; i >=-20; i --){
+  if (i % 2 === 0){
+    console.log(i);
+  }
+}
+
+// for( let i=10; i >=-20; i --){
+  //if (i % 2 == 0){
+   // console.log(i);
+//  }
+// }

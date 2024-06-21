b1de6d9db95eb2fe8ae3f7ff2d93e53de7f1d860
Rajat saraswat <149191138+kg-rajat@users.noreply.github.com>
2024-06-21 17:05:26 +0530

    Add files via upload
    day65

---
+++ Day65/index.js
@@ -0,0 +1,20 @@
+//Q1
+
+let  quarter = 4;
+
+switch (quarter) {
+  case 1:
+    console.log("One, One-hundred, One-thousand");
+    break;
+  case 2:
+      console.log("Two, Two-Hundred, Two-thousand");
+     break;
+  case 3:
+       console.log("Three, Three-hundred, three-thousand");
+       break;
+  case 4:
+       console.log("four, four-hundred, four-thousand");
+       break;
+       default:
+         console.log("wrong number")
+}

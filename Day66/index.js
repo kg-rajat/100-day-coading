986b3431223be0db98e328798fdbf7d239f537bb
Rajat saraswat <149191138+kg-rajat@users.noreply.github.com>
2024-06-21 17:06:37 +0530

    Add files via upload
    day 66

---
+++ Day66/index.js
@@ -0,0 +1,9 @@
+let str = "ILoveyou";
+
+console.log(str.indexOf ("Love"));
+console.log(str.indexOf ("o"));
+console.log(str.indexOf ("r"));
+
+let newMsg = "  Rajatsaraswat   ";
+
+console.log(newMsg.toLocaleLowerCase().trim());

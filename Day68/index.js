7e9f8a1f34a440449c7a08c20b6675c5e002dbae
Rajat saraswat <149191138+kg-rajat@users.noreply.github.com>
2024-06-22 11:01:53 +0530

    Add files via upload
    day 68

---
+++ Day68/index.js
@@ -0,0 +1,40 @@
+let str = "Programmerfect";
+
+console.log(str.slice(0,3));
+console.log(str.slice(10));
+console.log(str.slice(-2));
+
+
+let text = "microsoft";
+
+let newText = Text.replace("microsoft", "programmerfect");
+console.log(newText);
+
+// let msg = "Rajat saraswat";
+// console.log(msg.repeat(3));
+
+let msg = "help";
+console.log(msg.trim(), msg.toLocaleLowerCase());
+
+let name = "The helppur";
+
+console.log(name.slice(4,9));
+console.log(name.indexOf("pu"));
+console.log(name.replace("the", "welcome"));
+
+// Arry 
+let students = ["Rajat", "Doli", "Kisan"];
+console.log(students);
+console.log(students[0]);
+console.log(students[-1]);
+console.log(students[2]);
+
+let num = [2, 4, 6, 8, 10, 12, 14]
+console.log(num);
+console.log(num[2]);
+console.log(num[3]);
+console.log(num[4]);
+console.log(num[5]);
+console.log(num[8]);
+console.log(num.length);
+console.log(num.length);

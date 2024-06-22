4318cf9d74caffc6f2b4788fddfe32e8a2c7e0d5
Rajat saraswat <149191138+kg-rajat@users.noreply.github.com>
2024-06-22 11:03:08 +0530

    Add files via upload
    day 69

---
+++ Day69/index.js
@@ -0,0 +1,13 @@
+let marks = [99, 78, 76, 79, 66];
+let names =["Rajat", "Geeta", "Neha", "Doli", "anu"];
+let info = ["anuraj", 25, 6.5];
+
+console.log(marks);
+console.log(names);
+console.log(info);
+
+console.log(marks.length);
+console.log(marks.at(2));
+console.log(marks[3]);
+console.log(marks.shift());
+console.log(marks.unshift(78));

8d83629ea5417421d43ce45a346471aeb660ac00
Rajat saraswat <149191138+kg-rajat@users.noreply.github.com>
2024-06-22 11:15:31 +0530

    Add files via upload
    day 78

---
+++ Day78/index.js
@@ -0,0 +1,18 @@
+// Example - 1 
+
+function myFunc(theArr) {
+  theArr[0] =30;
+}
+
+const arr = [40];
+
+myFunc(arr);
+console.log(arr[0]);
+console.log(arr[0]);
+
+// Example - 2
+
+function bark() {
+  console.log("Wolf");
+}
+bark.animal = "Dog";

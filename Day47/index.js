4297e105aa160dfaad0514702b21b4cebb6835c0
Rajat saraswat <149191138+kg-rajat@users.noreply.github.com>
2024-06-25 17:46:01 +0530

    Add files via upload
    day 47

---
+++ Day47/index.js
@@ -0,0 +1,18 @@
+let fullName = "Richard Rahul";
+let age = 24;
+
+console.log(fullName)
+
+typeof fullName;
+
+a = 40
+b = 60
+
+sum = a+b
+console.log(sum)
+
+c = 40
+d = 60
+sum = c+d
+
+console.log(sum)
